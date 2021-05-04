fc83d4285d64858feeae9a247cf4a67cbc8e20ea
Maksim <maksimshalaev11@gmail.com>
2021-05-04 11:33:54 +0300

    2

--- MainV1.java
+++ MainV1.java
@@ -1,8 +1,8 @@
 public class MainV1 {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = 10_000;
-        int miles = service.calculate(cost);
+        int price = 220_000;
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
